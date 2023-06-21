70db0e7e343e02fc7de658c87d9efbb46f714df0
HiFiBerry <info@hifiberry.com>
2023-06-21 07:10:01 +0000

    Bump version, add installation of Serum lib

--- buildroot/package/dmdreader/dmdreader.mk
+++ buildroot/package/dmdreader/dmdreader.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-DMDREADER_VERSION = 499331e5ad95d2ef0af2261ea06d2714f065c659
+DMDREADER_VERSION = ce9c41b8acb115bb395143b2df178f24e21eae98
 DMDREADER_SITE = https://github.com/pinballpower/code_dmdreader
 DMDREADER_SITE_METHOD = git
 DMDREADER_GIT_SUBMODULES = YES
 
 DMDREADER_DEPENDENCIES = boost rpi-firmware libgpiod
+
+define DMDREADER_POST_INSTALL
+ $(INSTALL) -D -m 0755 $(@D)/external/libserum/libSerum.so $(TARGET_DIR)/usr/lib/
+endef
+
+DMDREADER_POST_INSTALL_TARGET_HOOKS = DMDREADER_POST_INSTALL
 
 $(eval $(cmake-package))
