a7ac477d3ca90666c7504e23ad4cc1ddb1c4287c
Pinball Power <info@pinball-power.com>
2023-06-25 18:18:01 +0000

    DMDReader package now working including graphics packages

--- buildroot/package/dmdreader/dmdreader.mk
+++ buildroot/package/dmdreader/dmdreader.mk
@@ -4,25 +4,19 @@
 #
 ################################################################################
 
-DMDREADER_VERSION = e1e448f7ca11a85241abc3a5e0b3cf9faed97128
+DMDREADER_VERSION = 3707916f0e2f65856d3e50801b44bdbf8a1adde4
 DMDREADER_SITE = https://github.com/pinballpower/code_dmdreader
 DMDREADER_SITE_METHOD = git
 DMDREADER_GIT_SUBMODULES = YES
 
-DMDREADER_DEPENDENCIES = boost rpi-firmware libgpiod 
+DMDREADER_DEPENDENCIES = boost rpi-firmware libgpiod
+
+DMDREADER_CONF_OPTS += -DFORCE_SPI=1 -DFORCE_LEDMATRIX=1 -DFORCE_SERUM=1
 
 ifeq ($(BR2_PACKAGE_DMDREADER_OGL),y)
-DMDREADER_DEPENDENCIES += libdrm mesa3d
+DMDREADER_DEPENDENCIES += libdrm mesa3d ffmpeg
+DMDREADER_CONF_OPTS += -DFORCE_OPENGLES=1
 endif
-
-define MYCONFIG
-	echo $(@D)
-	cp $(BR2_EXTERNAL_PBOS_PATH)/../CMakeLists.txt-dmdreader $(@D)
-endef
-
-define CONFIGDONE
-	sleep 100
-endef
 
 define DMDREADER_POST_INSTALL
  $(INSTALL) -D -m 0755 $(@D)/external/libserum/libSerum.so $(TARGET_DIR)/usr/lib/
@@ -32,9 +26,6 @@ define DMDREADER_INSTALL_INIT_SYSV
     $(INSTALL) -D -m 755 $(BR2_EXTERNAL_PBOS_PATH)/package/dmdreader/dmdreader.init $(TARGET_DIR)/etc/init.d/S11dmdreader
 endef
 
-
 DMDREADER_POST_INSTALL_TARGET_HOOKS = DMDREADER_POST_INSTALL
-DMDREADER_POST_EXTRACT_HOOKS = MYCONFIG
-DMDREADER_POST_CONFIGURE_HOOKS = CONFIGDONE
 
 $(eval $(cmake-package))
