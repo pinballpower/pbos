b5b9af3c44d24779a6c41ebab2c5d97085a0175c
Pinball Power <info@pinball-power.com>
2023-07-04 07:32:23 +0000

    Bump to latest version

--- buildroot/package/dmdreader-pico/dmdreader-pico.mk
+++ buildroot/package/dmdreader-pico/dmdreader-pico.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-DMDREADER_PICO_VERSION=v0.1-alpha
-DMDREADER_PICO_BINARY_NAME=dmdreader.elf
+DMDREADER_PICO_VERSION=0.1.2
+DMDREADER_PICO_BINARY_NAME=dmdreader-$(DMDREADER_PICO_VERSION).elf
 DMDREADER_PICO_SOURCE=$(DMDREADER_PICO_BINARY_NAME)
-DMDREADER_PICO_SITE=https://github.com/pinballpower/code_dmd/releases/download/$(DMDREADER_PICO_VERSION)
+DMDREADER_PICO_SITE=https://github.com/pinballpower/code_dmd/releases/download/v$(DMDREADER_PICO_VERSION)
 
 define DMDREADER_PICO_EXTRACT_CMDS
 endef
 
 define DMDREADER_PICO_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/firmware
-    $(INSTALL) -D -m 0644 $(DMDREADER_PICO_DL_DIR)/$(DMDREADER_PICO_BINARY_NAME) $(TARGET_DIR)/firmware
+    $(INSTALL) -D -m 0644 $(DMDREADER_PICO_DL_DIR)/$(DMDREADER_PICO_BINARY_NAME) $(TARGET_DIR)/firmware/dmdreader.elf
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_PBOS_PATH)/package/dmdreader-pico/program-rp2040.sh \
                 $(TARGET_DIR)/firmware
     $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_PBOS_PATH)/package/dmdreader-pico/raspberrypi-swd-dmdreader.cfg \
