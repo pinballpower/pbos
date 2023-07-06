dda4ac2d116fd43f54b414e90a27dc8d12469ec0
Pinball Power <info@pinball-power.com>
2023-07-06 07:29:40 +0000

    Add Pi4 KMS configuration

--- buildroot/package/configtxt/configtxt.mk
+++ buildroot/package/configtxt/configtxt.mk
@@ -10,6 +10,14 @@ define CONFIGTXT_INSTALL_TARGET_CMDS
         echo "# Enable SPI" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 	echo "dtparam=i2c=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 	echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+	echo "boot_delay=0" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "disable_splash=1" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "force_eeprom_read=0" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "force_turbo=0" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "initial_turbo=60" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "[pi4]" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "dtoverlay=vc4-kms-v3d" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "max_framebuffers=2" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
 $(eval $(generic-package))
